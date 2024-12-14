1e450e004de5ce77b8692c3ee7143cfc251591d8
tallpaul3486 <tallpaul3486@users.noreply.github.com>
2024-12-13 21:43:53 -0500

    Initial Commit

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Paul Smith on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipesAppTests/RecipesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppTests.swift
+//  RecipesAppTests
+//
+//  Created by Paul Smith on 12/13/24.
+//
+
+import Testing
+@testable import RecipesApp
+
+struct RecipesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
